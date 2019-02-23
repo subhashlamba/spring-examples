5aaaa3a92f2e22c254f3f6b121b99c2b01de226b
subhash <subhashlammba@gmail.com>
2019-02-23 10:42:25 +0530

    Added example for Spring boot + HikariCP

---
+++ spring-boot-HikariCP-example/src/main/java/com/javadeveloperzone/dao/EmployeeDAO.java
@@ -0,0 +1,35 @@
+package com.javadeveloperzone.dao;
+
+import com.javadeveloperzone.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Created by JavaDeveloperZone on 03-08-2017.
+ */
+@Component
+public class EmployeeDAO {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+
+    public java.util.List<Employee> getEmployeeList() {
+        List<Map<String, Object>> employees = jdbcTemplate.queryForList("select * from employee");
+        return employees.stream().map(e -> {
+            Employee employee = new Employee();
+            employee.setEmployeeName((String)e.get("employeeName"));
+            employee.setEmployeeId((Integer) e.get("employeeId"));
+            employee.setEmployeeRole((String) e.get("employeeRole"));
+            return employee;
+        }).collect(Collectors.toList());
+    }
+}
