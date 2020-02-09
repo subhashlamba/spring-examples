5f68200e7493f0acc5335031edc0543c26d8d2b7
iampolireddy <iampolireddy@gmail.com>
2020-02-09 11:57:34 +0530

    Springboot+mysql+cloud foundry example

---
+++ SpringBootMysqlCF/src/main/java/com/javadevloperzone/SpringBootMysqlCfApplication.java
@@ -0,0 +1,13 @@
+package com.javadevloperzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlCfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMysqlCfApplication.class, args);
+	}
+
+}
---
+++ SpringBootMysqlCF/src/main/java/com/javadevloperzone/services/UserRepository.java
@@ -0,0 +1,8 @@
+package com.javadevloperzone.services;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+	
+}
